152745fb9b1cfe117e13094bcca56f32e01e4168
Anis Alkomem <anis.alkumaim@gmail.com>
2020-05-04 19:22:40 +0200

    Moved the project from the Week1 homework folder to the homework folder

--- homework/index.js
+++ homework/index.js
@@ -7,6 +7,7 @@
     xhr.responseType = 'json';
     xhr.onload = () => {
       if (xhr.status >= 200 && xhr.status <= 299) {
+        console.log(xhr.response);
         cb(null, xhr.response);
       } else {
         cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
@@ -29,22 +30,50 @@
     return elem;
   }
 
-  function renderRepoDetails(repo, ul) {
-    createAndAppend('li', ul, { text: repo.name });
+  function renderRepoDetails(repo, root) {
+    const card = createAndAppend('div', root, { class: 'card' });
+    // The usage of table was selected because the data was tabular. Additionally, I relied on inner HTML to format the data because it was unconventional, i.e. one is a link, in the other data the time should be fomratted. So, there was an option of looping through two arrays of items and their description, but I thought it would not save code or effort.
+    const infoTable = createAndAppend('table', card);
+    infoTable.innerHTML = `
+      <tr>
+    <td>Repository: </td>
+    <td><a href=${repo.html_url}>${repo.name}</a></td>
+  </tr>
+  <tr>
+    <td>Description: </td>
+    <td>${repo.description}</td>
+  </tr>
+  <tr>
+    <td>Forks: </td>
+    <td>${repo.forks_count}</td>
+  </tr>
+  <tr>
+    <td>Updated: </td>
+    <td>${repo.updated_at.replace(/[ tz]/gi, ' ')}</td>
+  </tr>
+    `;
   }
 
   function main(url) {
     fetchJSON(url, (err, repos) => {
       const root = document.getElementById('root');
+      createAndAppend('p', root, {
+        text: 'HYF Repositories',
+        class: 'header',
+      });
+
       if (err) {
-        createAndAppend('div', root, {
+        createAndAppend('p', root, {
           text: err.message,
           class: 'alert-error',
         });
         return;
       }
-      const ul = createAndAppend('ul', root);
-      repos.forEach(repo => renderRepoDetails(repo, ul));
+      // Because The assignment didn't indicate the basis for which the 10 repos are chosen, and also hinted on using sort and locale compare, I assumed alphabetically by name.
+      repos
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .slice(0, 10)
+        .forEach(repo => renderRepoDetails(repo, root));
     });
   }
 
